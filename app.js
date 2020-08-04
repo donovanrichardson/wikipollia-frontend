ad68ac0c6e4cb5d51cfad766f200726eea0f9120
donovanrichardson <donovanspqa@gmail.com>
2020-08-03 20:56:34 -0400

    having issues posting

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 async function render(){
-    const response = await fetch('https://wikipollia.herokuapp.com/')
-    const value = await response.json()
-    $('body').text(value.hello)
-
+    $('#articles').empty()
+    const response = await fetch('https://wikipollia.herokuapp.com/article')
+    const articles = await response.json()
+    articles.forEach(a=>{
+        $('#articles').append($('<li>').text(a))
+    })
 }
 
+async function sendVote(event){
+    const theVote = {
+        up:true,
+        oldid:1,
+        article:$('#title').val(),
+        comment:$('#commnet').val(),
+    }
+    console.log(theVote);
+    //snippet from https://stackoverflow.com/a/29823632/9608521
+    // try{
+        const rawResponse = await fetch('https://wikipollia.herokuapp.com/vote', {
+            method: 'POST',
+            // headers: {
+            //   'Accept': 'application/json',
+            //   'Content-Type': 'application/json'
+            // },
+            body: JSON.stringify(theVote)
+          });
+        //   const content = await rawResponse.json();
 
+    // }catch(err){
+    //     console.error(err)
+    // }
+
+      render()
+}
+
+$('form button').on('click',sendVote)
 render()
