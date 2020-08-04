b6fd52b54d989129573822fcfdf7035d255466f8
donovanrichardson <donovanspqa@gmail.com>
2020-08-04 10:23:05 -0400

    it werks here

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 async function render(){
     $('#articles').empty()
+    const newArticle = {
+        up:1,
+        oldid:1,
+        article:new Date().toString(),
+        comment:new Date().toString()
+    }
+    // console.log(
+    const postreq = await fetch('http://localhost:3000/vote',{
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(newArticle)
+    });
+    console.log(await postreq.json())
     const response = await fetch('http://localhost:3000/article')
     const articles = await response.json()
     articles.forEach(a=>{
@@ -17,5 +32,5 @@ async function sendVote(event){
     render()
 }
 
-$('form button').on('click',sendVote)
+// $('form button').on('click',sendVote)
 render()
