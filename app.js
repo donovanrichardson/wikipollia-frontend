2c6c27598203fd747881134e007412671bc7a0b0
donovanrichardson <donovanspqa@gmail.com>
2020-08-04 11:49:51 -0400

    the basic front/back end  works now

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
 async function render(){
     $('#articles').empty()
-    const newArticle = {
-        up:1,
-        oldid:1,
-        article:new Date().toString(),
-        comment:new Date().toString()
-    }
-    // console.log(
-    const postreq = await fetch('http://localhost:3000/vote',{
-    method:"POST",
-    headers:{
-        "Content-Type":"application/json"
-    },
-    body:JSON.stringify(newArticle)
-    });
-    console.log(await postreq.json())
-    const response = await fetch('http://localhost:3000/article')
+
+    const response = await fetch('https://wikipollia.herokuapp.com/article')
     const articles = await response.json()
     articles.forEach(a=>{
         $('#articles').append($('<li>').text(a))
     })
 }
 
-async function sendVote(event){
-    console.log(event);
-    const response = await fetch('http://localhost:3000/vote', {
-        method:"POST",
+async function doPost(obj){
+    // console.log(
+        try{
+            const postreq = await fetch('https://wikipollia.herokuapp.com/vote',{
+    method:"POST",
+    headers:{
         "Content-Type":"application/json"
-    })
-    console.log(await response.json());
-    render()
+    },
+    body:JSON.stringify(obj)
+    });
+    console.log(await postreq.json())
+        }catch(err){
+            console.error(err);
+        }
 }
 
-// $('form button').on('click',sendVote)
+async function sendVote(event){
+    console.log(event);
+    event.preventDefault()
+    console.log($('#title').val(),$('#comment').val());
+    myobj = {
+        up:1,
+        oldid:1,
+        article:$('#title').val(),
+        comment:$('#comment').val()
+    }
+    await doPost(myobj)
+}
+
+$('form button').on('click', async (event)=>{
+    event.preventDefault()
+    await doPost({
+        up:1,
+        oldid:1,
+        article:$('#title').val(),
+        comment:$('#comment').val()
+    })
+    /* await  */render()
+})
 render()
